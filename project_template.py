8656c20c4dd6962583b5f82ff0a11a6ce37eb2ef
Cristina Munteanu <cristina.munteanu@windriver.com>
2017-04-05 13:45:58 +0300

    Correlation matrix

--- project_template.py
+++ project_template.py
@@ -59,7 +59,19 @@ print(dataset.corr(method='pearson'))
 #pyplot.show()
 
 #scatter plot matrix
-scatter_matrix(dataset)
+#scatter_matrix(dataset)
+#pyplot.show()
+
+#correlation matrix
+fig = pyplot.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(dataset.corr(), vmin=-1, vmax=1, interpolation='none')
+fig.colorbar(cax)
+ticks = numpy.arange(0, 14,1)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.set_xticklabels(names)
+ax.set_yticklabels(names)
 pyplot.show()
 
 
