79b147cb81c6b0b2041432d6b0020173738f5135
Cristina Munteanu <cristina.munteanu@windriver.com>
2017-04-06 12:25:03 +0300

    Algorithm selection

--- project_template.py
+++ project_template.py
@@ -72,7 +72,7 @@ ax.set_xticks(ticks)
 ax.set_yticks(ticks)
 ax.set_xticklabels(names)
 ax.set_yticklabels(names)
-pyplot.show()
+#pyplot.show()
 
 
 
@@ -83,15 +83,105 @@ pyplot.show()
 
 # 4. Evaluate Algorithms
 # a) Split-out validation dataset
+array = dataset.values
+X=array[:, 0:13]
+Y=array[:,13]
+validation_size=0.20
+seed =7
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
 # b) Test options and evaluation metric
+num_folds = 10
+seed =7
+scoring = 'neg_mean_squared_error'
 # c) Spot Check Algorithms
 # d) Compare Algorithms
+models = []
+models.append(('LR', LinearRegression()))
+models.append(('LASSO', Lasso()))
+models.append(('EN', ElasticNet()))
+models.append(('KNN', KNeighborsRegressor()))
+models.append(('CART', DecisionTreeRegressor()))
+models.append(('SVR', SVR()))
+
+#evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = KFold(n_splits=num_folds, random_state=seed)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+	
+# Standardize the dataset
+pipelines = []
+pipelines.append(('ScaledR', Pipeline([('Scaler', StandardScaler()), ('LR', LinearRegression())])))
+pipelines.append(('ScaledLASSO', Pipeline([('Scaler', StandardScaler()), ('LASSO', Lasso())])))
+pipelines.append(('ScaledEN', Pipeline([('Scaler', StandardScaler()), ('EN', ElasticNet())])))
+pipelines.append(('ScaledKNN', Pipeline([('Scaler', StandardScaler()),('KNN', KNeighborsRegressor())])))
+pipelines.append(('ScaledCART', Pipeline([('Scaler', StandardScaler()),('CART',DecisionTreeRegressor())])))
+pipelines.append(('ScaledSVR', Pipeline([('Scaler', StandardScaler()),('SVR',SVR())])))
+
+results = []
+names = []
+for name, model in pipelines:
+	kfold = KFold(n_splits = num_folds, random_state = seed)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+	
+
+fig = pyplot.figure()
+fig.suptitle('Scaled Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
 
 # 5. Improve Accuracy
 # a) Algorithm Tuning
+# KNN Algorithm tunning
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+k_values = numpy.array([1,3,5,7,9,11,13,15,17,19,21])
+param_grid = dict(n_neighbors=k_values)
+model = KNeighborsRegressor()
+kfold = KFold(n_splits = num_folds, random_state = seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result=grid.fit(rescaledX, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+	print("%f (%f) with: %r" % (mean, stdev, param))
+
 # b) Ensembles
+ensembles = []
+ensembles.append(('ScaledAB', Pipeline([('Scaler', StandardScaler()), ('AB', AdaBoostRegressor())])))
+ensembles.append(('ScaledGBM', Pipeline([('Scaler', StandardScaler()), ('GBM', GradientBoostingRegressor())])))
+ensembles.append(('ScaledRF', Pipeline([('Scaler', StandardScaler()), ('RF', RandomForestRegressor())])))
+ensembles.append(('ScaledET', Pipeline([('Scaler', StandardScaler()),('ET', ExtraTreesRegressor())])))
+results = []
+names = []
+
+for name, model in ensembles:
+	kfold = KFold(n_splits = num_folds, random_state=seed)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	
+	print(msg)
+
 
 # 6. Finalize Model
 # a) Predictions on validation dataset
 # b) Create standalone model on entire training dataset
 # c) Save model for later use
+
