bc1c3780c30d5abe1d765ad26abffccb29c61350
Cristina Munteanu <cristina.munteanu@windriver.com>
2017-04-07 17:38:06 +0300

    Started working on Connectionist Bench (Sonar, Mines vs. Rocks) Data Set

---
+++ connectionist_bench_sonar_dataset/model.py
@@ -0,0 +1,56 @@
+# Python Project Template
+
+# 1. Prepare Problem
+# a) Load libraries
+
+import numpy
+from matplotlib import pyplot
+from pandas import read_csv
+from pandas import set_option
+from pandas.tools.plotting import scatter_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+# b) Load dataset
+url = 'sonar.all-data.csv'
+dataset = read_csv(url, header=None)
+
+# 2. Summarize Data
+# a) Descriptive statistics
+# b) Data visualizations
+
+# 3. Prepare Data
+# a) Data Cleaning
+# b) Feature Selection
+# c) Data Transforms
+
+# 4. Evaluate Algorithms
+# a) Split-out validation dataset
+# b) Test options and evaluation metric
+# c) Spot Check Algorithms
+# d) Compare Algorithms
+
+# 5. Improve Accuracy
+# a) Algorithm Tuning
+# b) Ensembles
+
+# 6. Finalize Model
+# a) Predictions on validation dataset
+# b) Create standalone model on entire training dataset
+# c) Save model for later use
